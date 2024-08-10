eb08a8214b6f9adea70d6d4d015a9e1e22fb933c
KroLL <vadimpaj@gmail.com>
2024-08-10 00:37:35 -0400

    Create ext.js

---
+++ ext.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Add site location for Safari custom CSS
+// @name:ru     Адрес сайта в data-атрибуте
+// @description Adds data-attribute of current site location
+// @description:ru Добавление адреса сайта в data-атрибут для доступа к нему из CSS в кастомных стилях для Safari
+// @namespace   https://github.com/vadimpaj
+// @author      vadimpaj
+// @update      
+// @version     1.0
+// @match       *://*/*
+// @grant       none
+
+// ==/UserScript==
+(function () {
+  'use strict';
+
+  function addDataLocationAttribute() {
+    document.documentElement.setAttribute(
+      'data-location',
+      window.location.hostname
+    );
+  }
+
+  document.addEventListener('DOMContentLoaded', addDataLocationAttribute);
+})();
