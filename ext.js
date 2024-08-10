7dbbff0ad243106332b5d2705a3f093f5101714e
KroLL <vadimpaj@gmail.com>
2024-08-10 01:00:46 -0400

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,39 +1,22 @@
 // ==UserScript==
-// @name        Add site location for Safari custom CSS
+// @name        Add Domain to the document root data-attribute
 // @name:ru     Адрес сайта в data-атрибуте
-// @description Adds data-attribute of current site location
+// @description Adds data-attribute of current domain to the document root for using in custom css
 // @description:ru Добавление адреса сайта в data-атрибут для доступа к нему из CSS в кастомных стилях для Safari
 // @namespace   https://github.com/vadimpaj
 // @author      vadimpaj
 // @update      https://github.com/vadimpaj/safari-location-attr-in-root/raw/main/ext.js
-// @version     1.1
+// @version     1.2
 // @match       *://*/*
 // @grant       none
-
 // ==/UserScript==
-(function () {
-  'use strict';
 
-  // Function to add or update data-location attribute on the HTML element
-  function addDataLocationAttributeToHtml() {
-    const htmlElement = document.documentElement;
-    let currentDomain = window.location.hostname;
-        
-    if (!htmlElement.hasAttribute('data-location')) {
-      // If 'data-location' does not exist, create it with the current domain
-      htmlElement.setAttribute('data-location', currentDomain);
-    } else {
-      // If 'data-location' already exists, update it to include the current domain if necessary
-      let existingLocation = htmlElement.getAttribute('data-location');
-      if (!existingLocation.includes(currentDomain)) {
-        htmlElement.setAttribute('data-location', existingLocation + ' ' + currentDomain);
-      }
-    }
-  }
+(function() {
+    'use strict';
 
-  // Run the function when the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', addDataLocationAttributeToHtml);
+    // Get the current domain
+    const currentDomain = window.location.hostname;
 
-  // Optionally, you can run this script on every page load or navigation
-  setInterval(addDataLocationAttributeToHtml, 5000); // Runs every 5 seconds
+    // Set the domain to the data-location attribute on the <html> element
+    document.documentElement.setAttribute('data-location', currentDomain);
 })();
